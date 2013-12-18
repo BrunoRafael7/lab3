512ca7ae2d4946bfd71ccddd3768c5d965bef1ff
BrunoRafael7 <bruno.rafha@gmail.com>
2013-12-18 02:49:34 -0300

    consertando erros

--- DisciplinesManager/app/models/Periodo.java
+++ DisciplinesManager/app/models/Periodo.java
@@ -1,14 +1,14 @@
-package model;
+package models;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public class Periodo {
 	
-	private List<Disciplinas> disciplinas;//não faz sentido ter um atributo do tipo List<GradeCurricular>
+	private List<Disciplina> disciplinas;
 	
 	public Periodo(){
-		disciplinas = new LinkedList<Disciplinas>();
+		disciplinas = new LinkedList<Disciplina>();
 	}
 	
 	public Object getDisciplinas() {
