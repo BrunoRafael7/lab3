b038ce25695fe23d155305897e5c32a2a313b042
Julio <julio.silva@ccc.ufcg.edu.br>
2013-12-19 02:31:40 -0300

    alterando testes e corrigindo arquivo

--- DisciplinesManager/test/DisciplinaTest.java
+++ DisciplinesManager/test/DisciplinaTest.java
@@ -1,17 +1,35 @@
+
+
 import static org.junit.Assert.*;
+import models.ColetorDeDisciplinas;
+import models.Disciplina;
 
-
+import org.junit.Before;
 import org.junit.Test;
 
-
 public class DisciplinaTest {
-
+	ColetorDeDisciplinas coletor;
+	Disciplina calculo2;
+	
+	
+	@Before
+	public void start(){
+		coletor = new ColetorDeDisciplinas();
+		coletor.coletar();
+		calculo2 = coletor.coletar().get(6);
+		
+	}
+	
 	@Test
-	public void testaQuantidadeDeCreditosDaDisciplina() {
-		//assertEquals(4, PROGRAMACAO1.getCredito());
-		//assertEquals(4, LABORATORIODEPROGRAMACAO1.getCredito());
+	public void deveVerifiarOsPreRequisitos(){
+		System.out.println(calculo2.getPreRequisitos());
+		assertEquals("" , calculo2.getPreRequisitos().toString());
 	}
 	
 	
-
+	@Test
+	public void deveVerificarQuantidadeDeCreditos(){
+		
+	}
+	
 }
