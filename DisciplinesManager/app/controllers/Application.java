24430fc9aefa6b5b30f7c5636e4b37c76f6e134b
BrunoRafael7 <bruno.rafha@gmail.com>
2013-12-18 22:02:44 -0300

    programa compilando e executando

--- DisciplinesManager/app/controllers/Application.java
+++ DisciplinesManager/app/controllers/Application.java
@@ -1,15 +1,24 @@
 package controllers;
 
-import models.Disciplina;
-import play.data.Form;
+import org.h2.table.Plan;
+
+import models.PlanoDeCurso;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+/*
+ * INFORMATION EXPERT : A classe Application é uma classe também controladora só que 
+ * esta é responsável apenas por prover os objetos "Result" para
+ * o lado cliente
+ */
+/*
+ *ALTA COESÃO/BAIXO ACOPLAMENTO : A classe Application e a classe 
+ */
 public class Application extends Controller{
 	
-	//private static Form<Disciplina> taskForm = Form.form(Disciplina.class);
+	private static PlanoDeCurso planoDeCurso = new PlanoDeCurso();
 	
 	public static Result index(){
-		return ok();
+		return ok(views.html.index.render(planoDeCurso.getPeriodos()));
 	}
 }
--- DisciplinesManager/test/PlanoDeCursoTest.java
+++ DisciplinesManager/test/PlanoDeCursoTest.java
@@ -15,12 +15,14 @@ public class PlanoDeCursoTest {
 	}
 	@Test
 	public void deveRetornarTodasAsDisciplinas(){
+		
 		assertEquals("Leitura_e_Produção_de_Textos", planoDeCurso.getPeriodos().get(0).getDisciplinas().get(0).getNome());
 		assertEquals("Álgebra_Vetorial_e_Geometria_Analítica", planoDeCurso.getPeriodos().get(0).getDisciplinas().get(1).getNome());
 		assertEquals("Cálculo_Diferencial_e_Integral_I", planoDeCurso.getPeriodos().get(0).getDisciplinas().get(2).getNome());
 		assertEquals("Programação_I",planoDeCurso.getPeriodos().get(0).getDisciplinas().get(3).getNome());
 		assertEquals("Introdução_à_Computação", planoDeCurso.getPeriodos().get(0).getDisciplinas().get(4).getNome());
 		assertEquals("Laboratório_de_Programação_I",planoDeCurso.getPeriodos().get(0).getDisciplinas().get(5).getNome());
+		
 	}
 
 }
